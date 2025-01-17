a878126d764ddfbe1eaa2f17eef9006e8dad4457
jamesphillipdeguzman <jamesphillipdeguzman@gmail.com>
2025-01-17 20:52:47 +0800

    Merge remote-tracking branch 'upstream/main'

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,23 +1,54 @@
-function convertToJson(res) {
-    if (res.ok) {
-        return res.json();
-    } else {
-        throw new Error("Bad Response");
-    }
-}
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 
 export default class ProductDetails {
-    constructor(product) {
-        this.product = product;
-        this.path = `../product_pages/${this.product}.html`;
+    constructor(productId, dataSource) {
+        this.productId = productId;
+        this.product = {};
+        this.dataSource = dataSource;
     }
-    getData() {
-        return fetch(this.path)
-            .then(convertToJson)
-            .then((data) => data);
+
+    // Initialize the page and add event listeners
+    init() {
+        // Add listener to "Add to Cart" button
+        document.getElementById("addToCart").addEventListener("click", (e) => this.addToCartHandler(e));
     }
-    async findProductById(id) {
-        const products = await this.getData();
-        return products.find((item) => item.Id === id);
+
+    // Add to cart button event handler
+    async addToCartHandler(e) {
+        // Find the product by Id
+        const product = await this.dataSource.findProductById(this.productId);
+
+        console.log(product);
+
+        // Add the product to the cart
+        this.addProductToCart(product);
     }
+
+    // Add product to the shopping cart
+    addProductToCart(product) {
+
+
+        // Check if there's an existing cart
+        let cart = getLocalStorage("cart") || []; // Get existing cart or initialize as an empty array
+
+        // Ensure cart is an array (if it's not, initialize it as an empty array) AI helped here
+        if (!Array.isArray(cart)) {
+            cart = [];
+        }
+
+        // If the cart is empty, push the product to the cart array and save it to local storage
+        if (cart.length === 0) {
+            cart.push(product);
+            setLocalStorage("cart", cart);
+        } else {
+            // If the cart is NOT empty, keep pushing the product to the cart array and log it to the console
+            cart.push(product);
+            console.log(cart);
+        }
+
+        // Update the contents of the cart in local storage
+        setLocalStorage("cart", cart);
+    }
+
+
 }
