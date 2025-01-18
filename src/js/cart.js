1000c475ca490471f6174da9f45469569ea4da72
Arcee Juan <arceetaraguajuan@gmail.com>
2025-01-18 23:44:21 +0800

    added remove from the cart feature

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,28 +1,44 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+    const cartItems = getLocalStorage("cart");
+
+    // Generate HTML for each cart item, passing the index to cartItemTemplate
+    const htmlItems = cartItems.map((item, index) => cartItemTemplate(item, index));
+    document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+    // Attach event listeners to all "X" buttons
+    document.querySelectorAll(".remove-item").forEach((button) => {
+        button.addEventListener("click", function () {
+            const itemIndex = this.dataset.index; // Get the index from the data-index attribute
+            removeItemFromCart(itemIndex);       // Remove the item from the cart
+        });
+    });
 }
 
-function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.image}"
-      alt="${item.brand}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.brand}</h2>
-  </a>
-  <p class="cart-card__color">${item.colors}</p> 
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.price}</p>
-</li>`;
-
-  return newItem;
+function cartItemTemplate(item, index) {
+    return `<li class="cart-card divider">
+        <span data-index="${index}" class="remove-item" style="color: red; cursor: pointer;">X</span>
+        <a href="#" class="cart-card__image">
+            <img src="${item.product.Image}" alt="${item.product.NameWithoutBrand}" />
+        </a>
+        <h2 class="card__name">${item.product.NameWithoutBrand}</h2>
+        <p class="cart-card__price">$${item.product.FinalPrice}</p>
+    </li>`;
 }
 
+function removeItemFromCart(itemIndex) {
+    const cartItems = getLocalStorage("cart");
+
+    // Remove the item at the specific index
+    cartItems.splice(itemIndex, 1);
+
+    // Save the updated cart back to local storage
+    setLocalStorage("cart", cartItems);
+
+    // Re-render the cart
+    renderCartContents();
+}
+
+// Initial render of cart contents
 renderCartContents();
