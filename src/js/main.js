684c2147654fbf1c5b7bb660a74241706e9e0237
jamesphillipdeguzman <jamesphillipdeguzman@gmail.com>
2025-01-19 22:21:40 +0800

    Updated files

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,7 @@ import { getParams, renderListWithTemplate } from "./utils.mjs";
 import ProductListing from "./ProductListing.mjs";
 import { list } from "postcss";
 
-
-
 document.addEventListener("DOMContentLoaded", () => {
-
   // Run the renderListWithTemplate function to load ALL the products from tents.json (e.g. all six of them)
   function renderListWithTemplate() {
     const dataSource = new ProductData("tents");
@@ -14,7 +11,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const listing = new ProductListing("Tents", dataSource, element);
     console.log(listing);
     listing.init();
-
   }
 
   renderListWithTemplate();
--- src/js/product.js
+++ src/js/product.js
@@ -66,7 +66,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // showTemplate();
 });
-
-
-
-
