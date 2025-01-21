964cf5ed08010ee8a824743a288e7c987e8c474f
jamesphillipdeguzman <jamesphillipdeguzman@gmail.com>
2025-01-21 21:37:10 +0800

    Fixed unused imports in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import ProductData from "./ProductData.mjs";
-import { getParams, renderListWithTemplate } from "./utils.mjs";
+// import { getParams, renderListWithTemplate } from "./utils.mjs";
 import ProductListing from "./ProductListing.mjs";
-import { list } from "postcss";
+// import { list } from "postcss";
 
 document.addEventListener("DOMContentLoaded", () => {
   // Run the renderListWithTemplate function to load ALL the products from tents.json (e.g. all six of them)
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const dataSource = new ProductData("tents");
     const element = document.querySelector(".product-list");
     const listing = new ProductListing("Tents", dataSource, element);
-    console.log(listing);
+    // console.log(listing);
     listing.init();
   }
 
