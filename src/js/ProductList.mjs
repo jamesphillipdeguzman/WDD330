af20047dcadd83f5978a32a949544ebd31f67ce8
jamesphillipdeguzman <jamesphillipdeguzman@gmail.com>
2025-01-22 00:07:12 +0800

    Updated ProductListing.mjs to ProductList.mjs

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,48 @@
+
+import { renderListWithTemplate } from "./utils.mjs";
+
+// Filter unique product names
+function filteredProducts(products) {
+    const uniqueProducts = products.filter((item, index, self) =>
+        self.findIndex(product => product.Brand.Name === item.Brand.Name) === index);
+
+    // console.log(uniqueProducts);
+    return uniqueProducts;
+}
+
+function productCardTemplate(product) {
+
+    return `<li class="product-card">
+      <a href="product_pages/index.html?product=${product.Id}">
+        <img src="${product.Image}" alt="Image of ${product.Name}">
+        <h3 class="card__brand">${product.Brand.Name}</h3>
+        <h2 class="card__name">${product.Name}</h2>
+        <p class="product-card__price">$${product.FinalPrice}</p>
+      </a>
+    </li>`
+}
+
+export default class ProductList {
+    // Make the ProductListing class as flexible and reusable by passing in category, dataSource, and listElement.
+    constructor(category, dataSource, listElement) {
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+    // Use init function to initialize and grab the dataSource using the getData function from ProductData class.
+    async init() {
+        const productList = await this.dataSource.getData()
+
+        // List only those products with valid images
+        const filteredList = filteredProducts(productList);
+        // Render list of products here...
+        this.renderList(filteredList);
+    }
+
+    renderList(productList) {
+        // console.log(productList);
+        renderListWithTemplate(productCardTemplate, this.listElement, productList);
+
+    }
+
+}
