b485f4743290c82e5a07e7b253f2821c38294815
jamesphillipdeguzman <jamesphillipdeguzman@gmail.com>
2025-01-26 17:04:10 +0800

    Updated folder name from product_pages to product-pages

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -13,7 +13,7 @@ function filteredProducts(products) {
 function productCardTemplate(product) {
 
     return `<li class="product-card">
-      <a href="product_pages/index.html?product=${product.Id}">
+      <a href="product-pages/index.html?product=${product.Id}">
         <img src="${product.Image}" alt="Image of ${product.Name}">
         <h3 class="card__brand">${product.Brand.Name}</h3>
         <h2 class="card__name">${product.Name}</h2>
