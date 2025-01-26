cbf6450712f91946dad0248274bda8dc0a479d4a
jamesphillipdeguzman <jamesphillipdeguzman@gmail.com>
2025-01-26 16:20:56 +0800

    Update config files

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     presets: [
-        "@babel/preset-env"
-
-    ],
+        [
+            "@babel/preset-env",
+            {
+                modules: false, // Keep ECMAScript modules as-is
+            }
+        ]
+    ]
 };
