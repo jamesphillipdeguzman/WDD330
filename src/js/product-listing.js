ac793d1740dbb9769dd9a94abe252ea608db3dac
jamesphillipdeguzman <jamesphillipdeguzman@gmail.com>
2025-01-28 23:18:02 +0800

    Fixed product listing

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -5,21 +5,16 @@ import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter, getParams } from "./utils.mjs";
 
-
-
-
 loadHeaderFooter();
-
-
 
 // Run the renderListWithTemplate function to load ALL the products from tents.json (e.g. all six of them)
 function renderListWithTemplate() {
-    debugger;
-    const category = getParams("category");
-    const dataSource = new ProductData(category); // ensure category is passed here...
-    const element = document.querySelector(".product-list");
-    const listing = new ProductList(category, dataSource, element);
-    // console.log(listing);
-    listing.init();
+  debugger;
+  const category = getParams("category");
+  const dataSource = new ProductData(category); // ensure category is passed here...
+  const element = document.querySelector(".product-list");
+  const listing = new ProductList(category, dataSource, element);
+  // console.log(listing);
+  listing.init();
 }
 renderListWithTemplate();
